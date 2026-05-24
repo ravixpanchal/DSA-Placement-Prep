999aa075158674feef32b123fe93eeade03dc169
ravixpanchal <ravi.panchal.kaithi@gmail.com>
2026-05-24 15:15:17 +0530

    Add files

---
+++ Array/Easy/11_union_of_two_sorted_array.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int>arr1={1,2,2,2,3,3,3,4};
+    vector<int>arr2={4,4,4,5,5,5,6,7};
+
+    int m=arr1.size();
+    int n=arr2.size();
+
+    vector<int>ans;
+
+    //2 pointer approach
+}
