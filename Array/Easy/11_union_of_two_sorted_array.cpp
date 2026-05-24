921b8b265b17000b7f987e70d3a206286a6e6f42
ravixpanchal <ravi.panchal.kaithi@gmail.com>
2026-05-24 15:32:58 +0530

    union of 2 sorted array

--- Array/Easy/11_union_of_two_sorted_array.cpp
+++ Array/Easy/11_union_of_two_sorted_array.cpp
@@ -11,4 +11,43 @@ int main(){
     vector<int>ans;
 
     //2 pointer approach
+    int i=0;
+    int j=0;
+
+    while(i<m && j<n){
+        if(arr1[i]<=arr2[j]){
+            if(ans.size()==0 || ans.back()!=arr1[i]){
+                ans.push_back(arr1[i]);
+
+            }
+            i++;
+        }else{
+            if(ans.size()==0 || ans.back()!=arr2[j]){
+                ans.push_back(arr2[j]);
+            }
+            j++;
+
+        }
+
+    }
+    while(i<m){
+            if(ans.size()==0 || ans.back()!=arr1[i]){
+                ans.push_back(arr1[i]);
+            }
+            i++;
+        }
+
+        while(j<n){
+            if(ans.size()==0 || ans.back()!=arr2[j]){
+                ans.push_back(arr2[j]);
+            }
+            j++;
+        }
+        for(auto &x: ans){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+
+
+        return 0;
 }
