b64877e759ddc5dc5017bcc383e94635304fe747
ravixpanchal <ravi.panchal.kaithi@gmail.com>
2026-05-26 12:29:32 +0530

    Add all easy problems

--- Array/Easy/13_missing_number.cpp
+++ Array/Easy/13_missing_number.cpp
@@ -2,15 +2,23 @@
 using namespace std;
 
 int main(){
-    vector<int>arr={1,2,4,5,6,7,8,9};
+    vector<int>nums={1,2,4,5,6,7,8};
 
-int xorr=0;
+    int xorr1=0,xorr2=0;
+    int n=nums.size();
 
-for(int i=1;i<arr.size();i++){
-    xorr^=arr[i];
-}
+    for(int i=0;i<=n+1;i++){
+        xorr1^=i;
+    }
 
-cout<<"missing no: "<<xorr<<endl;
+    for(auto x: nums){
+        xorr2^=x;
+    }
+
+    int ans=xorr1^xorr2;
+
+    cout<<"missing number is: "<<ans<<endl;
+
 
 
     return 0;
